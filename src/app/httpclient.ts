4d6c581c496e19be898434fef21e83d60b98ebb7
Synchro1099 <peredajanmark@gmail.com>
2026-02-02 15:55:19 +0800

    Updating the httpclientclient

--- src/app/httpclient.ts
+++ src/app/httpclient.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of, tap } from 'rxjs';
 import { User } from './user.model';
 import { Post } from './post.model'; // Add this line
 
@@ -8,13 +8,30 @@ import { Post } from './post.model'; // Add this line
   providedIn: 'root',
 })
 export class Httpclient {
+  private usersUrl = 'https://jsonplaceholder.typicode.com/users';
+  private Post = 'https://jsonplaceholder.typicode.com/posts'
   constructor(private http: HttpClient) {}
 
-  getUsersRemotely(): Observable<User[]> {
-    return this.http.get<User[]>('https://jsonplaceholder.typicode.com/users');
+
+ getUsersRemotely(): Observable<User[]> {
+    const cachedUsers = localStorage.getItem('users');
+    if (cachedUsers) {
+      return of(JSON.parse(cachedUsers));
+    }
+    return this.http.get<User[]>(this.usersUrl).pipe(
+      tap(users => localStorage.setItem('users', JSON.stringify(users)))
+    );
   }
 
-  getPostsRemotely(): Observable<Post[]> {
-    return this.http.get<Post[]>('https://jsonplaceholder.typicode.com/posts');
-  }
+ getPostsRemotely(): Observable<Post[]> {
+const cachedUsers = localStorage.getItem('post');
+
+    if (cachedUsers) {
+      return of(JSON.parse(cachedUsers));
+    }
+
+    return this.http.get<Post[]>(this.Post).pipe(
+  tap(posts => localStorage.setItem('post', JSON.stringify(posts)))
+ );
+}
 }
